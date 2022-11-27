28f971a11463ebc832c542738f168d65576a8025
Chuong Huynh <hnchuong@gmail.com>
2022-11-27 13:40:41 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.hosts << "rails-production-27d5.up.railway.app"
 end
